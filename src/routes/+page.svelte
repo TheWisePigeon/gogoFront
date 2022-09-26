1c0bc33eb12978c31015037390ac5450a65d708d
TheWisePigeon <zozozozeph@gmail.com>
2022-09-26 21:26:31 +0000

    feat: added heaeder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1 class=" bg-fuchsia-500">
-    Bruh
+<h1 class=" bg-green-500 flex justify-around  font-mono text-lg md:text-xl lg:text-2xl text-white">
+    <div>Go Go Short</div>
+    <button>
+        Login/logout/Account
+    </button>
 </h1>
+<div class=" flex justify-center gap-4 my-4">
+    <input class=" border-green-500 border-2 rounded" type="text" placeholder="Enter your link here">
+    <button>
+        shorten
+    </button>
+</div>
