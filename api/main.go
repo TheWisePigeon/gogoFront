9e5dc6c22bf82c8a94e8edb4108fb837fda432fc
TheWisePigeon <zozozozeph@gmail.com>
2022-09-28 10:43:42 +0000

    fix: removed return

--- api/main.go
+++ api/main.go
@@ -5,8 +5,6 @@ import (
   "net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) (result string) {
+func Handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-  result = "Sap nigga"
-  return
 }
